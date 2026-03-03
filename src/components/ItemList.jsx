33d77aeb751d6ecb67ce1687dac884048b275432
Udit Kumar <kudit223@gmail.com>
2026-03-03 18:04:23 +0530

    updated

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-
-function ItemList(){
-    const items=[1,2,3];
-    return(<div>
-    <h1>Item List</h1>
-    <ul>
-      {items.map((item)=>{
-        return <li key={item}><Link to= {`/items/${item}`}>item {item}</Link></li>
-      })}  
-    </ul>
-    </div>)
+const ItemList = () => {
+    const items = [1, 2, 3]; 
+  return (
+    <div>
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+            <li key={item}>
+           
+                <Link to={`/items/${item}`}>Item {item}</Link>
+            </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default ItemList;
