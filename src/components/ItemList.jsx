9a16ba847072f1e889a5bf2f6fdf9339f05df755
Udit Kumar <kudit223@gmail.com>
2026-03-03 17:53:28 +0530

    updated

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 
 
 function ItemList(){
+    const items=[1,2,3];
     return(<>
     <h1>Item List</h1>
     <ul>
-        <li><Link to="/items/1">item 1</Link></li>
-        <li><Link to="/items/2">item 2</Link></li>
-        <li><Link to="/items/3">item 3</Link></li>
+      {items.map((item,index)=>{
+        return <li key={index}><Link to= {`/items/${item}`} >item {item}</Link></li>
+      })}  
     </ul>
     </>)
 }
