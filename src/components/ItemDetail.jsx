15b5372668a2670cbbe2090968ab60a602d8741d
Udit Kumar <kudit223@gmail.com>
2026-03-03 17:39:29 +0530

    updated

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,10 +4,10 @@ import { useParams } from "react-router-dom";
 
 function ItemDetails(){
     const {id}= useParams();
-    return(<>
+    return(<div>
     <h1>{`Item ${id}`}</h1>
     <p>Description for Item {id}</p>
-    </>)
+    </div>)
 }
 
 export default ItemDetails;
